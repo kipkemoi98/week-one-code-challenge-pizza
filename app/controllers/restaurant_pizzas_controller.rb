dfead2d81f1ce5085c69a12b33cc896dc38cbcbf
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-11-29 09:11:47 +0300

    done

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantPizzasController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+        def create 
+           restaurant_pizza = RestaurantPizza(restaurant_pizza_params) 
+        end    render json: restaurant_pizza, status: :created
+    
+        private 
+        def restaurant_pizza_params
+            params.permit(:restaurant_id, :pizza_id, :price)
+        end  
+    
+        def unprocessable_entity_method(exception)
+            render json: (errors: exception.record.full_messages), status: :unprocessable_entity
+        end
+    
+    end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,28 @@
 class RestaurantsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
+        def index
+            
+    restaurants = Restaurant.all
+    render json: restaurants, status: :ok
+    end
+    def show
+        restaurant = Restaurant.find_by!(id: params[:id]
+        render json : restaurant, serializer: PizzaRestaurantSerializer, status: :ok
+    end
+    def destroy
+        restaurant = find_restaurant
+        restaurant.destroy
+        head :no_content
+      end
+    
+      private
+    
+      def find_restaurant
+        Restaurant.find(params[:id])
+      end
+    
+      def render_not_found_response
+        render json: { error: "Restaurant not found"}, status: :not_found
+      end
+    end
+    
